6940a859ecaeb0a73a3230a44cf53a132d37e91c
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-01-07 15:25:04 -0600

    Added Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import React from 'react'
+import githubImage from './assets/github.png'
+import linkedinImage from './assets/linkedin.png'
+import MediaIcon from './components/MediaIcon'
 import './App.css'
 
 function App() {
-  
-  
   return (
     <>
     <h1>Korbin Brooks</h1>
-    <body> C++ Java React Python </body>
+    <div> C++ Java React Python </div>
+    <MediaIcon image={githubImage} onClick={() => open('https://github.com/Korbanzo')}/>
+    <MediaIcon image={linkedinImage} onClick={() => open('https://www.linkedin.com/in/korbinbrooks')}/>
     </>
   )
 }
 
-export default App
+export default App;
