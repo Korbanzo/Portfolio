355b95409b691c3eb49cdb73598b5cd940528ff5
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-01-08 16:02:42 -0600

    Added resume media icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import './App.css'
 import githubImage from './assets/github.png'
 import linkedinImage from './assets/linkedin.png'
 import emailImage from './assets/email.png'
+import resumeImage from './assets/resume.png'
+import resumeDoc from './assets/MyResume.pdf'
 import MediaIcon from './components/MediaIcon'
 import ProjectBrowser from './components/ProjectBrowser'
-
-// Add resume mediaicon
 
 function App() {
   return (
     <>
     <h1>Korbin Brooks</h1>
     <div> C++ Java React Python </div>
+    <MediaIcon image={resumeImage} onClick={() => open(`${resumeDoc}`)}></MediaIcon>
     <MediaIcon image={githubImage} onClick={() => open('https://github.com/Korbanzo')}/>
     <MediaIcon image={emailImage} onClick={() => open("mailto:Brooks.m.Korbin@gmail.com")}/>
     <MediaIcon image={linkedinImage} onClick={() => open('https://www.linkedin.com/in/korbinbrooks')}/>
