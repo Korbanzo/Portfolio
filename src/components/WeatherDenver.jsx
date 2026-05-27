d6e12d750866d9188f2c4d605e6b2717239384fa
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-05-27 12:59:22 -0600

    Added All Weather Icons!

--- src/components/WeatherDenver.jsx
+++ src/components/WeatherDenver.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import ClearDay from "../assets/weathericons/clearday.png"
+import PartlyCloudy from "../assets/weathericons/partlycloudy.png"
+import Overcast from "../assets/weathericons/overcast.png"
+import Fog from "../assets/weathericons/fog.png"
+import Drizzle from "../assets/weathericons/drizzle.png"
+import Rain from "../assets/weathericons/rain.png"
+import Sleet from "../assets/weathericons/sleet.png"
+import Snow from "../assets/weathericons/snow.png"
+import ThunderStorm from "../assets/weathericons/thunderstorm.png"
 
 const URL = "https://api.open-meteo.com/v1/forecast?latitude=39.7392&longitude=-104.9847&models=gfs_seamless&current=temperature_2m,is_day,weather_code&timezone=America%2FDenver&forecast_days=1&wind_speed_unit=mph&precipitation_unit=inch&temperature_unit=fahrenheit"
 
@@ -31,7 +39,30 @@ const WeatherCodes = {
 }
 
 const WeatherIcons = {
-    "Clear": ClearDay
+    "Clear": ClearDay,
+    "Mainly Clear": PartlyCloudy,
+    "Partly Cloudy": PartlyCloudy,
+    "Overcast": Overcast,
+    "Fog": Fog,
+    "Depositing rime fog": Fog,
+    "Light Drizzle": Drizzle,
+    "Drizzle": Drizzle,
+    "Dense Drizzle": Drizzle,
+    "Light rain": Rain,
+    "Rain": Rain,
+    "Heavy rain": Rain,
+    "Light freezing rain": Sleet,
+    "Heavy freezing rain": Sleet,
+    "Light snow": Snow,
+    "Snow": Snow,
+    "Heavy snow": Snow,
+    "Snow grains": Snow,
+    "Light showers": Drizzle,
+    "Showers": Drizzle,
+    "Violent showers": Drizzle,
+    "Light snow showers": Snow,
+    "Heavy snow showers": Snow,
+    "Thunderstorms": ThunderStorm
 }
 
 const WeatherDenver = () => {
@@ -62,19 +93,19 @@ const WeatherDenver = () => {
         <>
             <h2 style={{ margin: '0 0 1rem 0' }}>Denver, Colorado</h2>
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: "space-between", width: '100%', gap: '1.5rem', alignItems: 'center'}}>
-                <div style={{backgroundImage: `url(${currentIcon})`, 
+                <div style={{
+                    backgroundImage: `url(${currentIcon})`, 
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     backgroundRepeat: 'no-repeat',
                     width: '75px',
                     height: '75px',
-                    borderRadius: '50%',
                     flexShrink: 0
                 }}/>
 
                 <div style={{display: 'flex', flexDirection: "column", gap: '0.5rem'}}>
                     <p style={{ margin: 0 }}>{weather.current.temperature_2m.toFixed(0)} {weather.current_units.temperature_2m}</p>
-                    <p style={{ margin: 0 }}>{currentWeather}</p>
+                    <p style={{ margin: 0}}>{currentWeather}</p>
                 </div>
             </div>
         </>
