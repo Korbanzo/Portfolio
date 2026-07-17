06a8a5e0c1135fdbb466767727e54f18c28fb3ca
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-07-17 15:00:55 -0600

    Changing some stylings

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,11 +1,9 @@
-import { div } from "three/tsl";
-
 const ToolBar = () => {
 
 
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', backgroundColor: 'rgb(0, 0, 0, 0.5)', height: '30px'}}>
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', backgroundColor: 'rgb(0, 0, 0, 0.5)', height: '30px', width: '100vw'}}>
 
             <button className="tool_bar_btn" onClick={() => {window.location.href = "#project_browser"}}>Projects</button>
             <button className="tool_bar_btn" onClick={() => {window.location.href = "/contact/contact.html"}}>That one</button>
