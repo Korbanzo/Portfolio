de47050fe2c198ca880ce693a677b9a1f3e6d895
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-07-17 15:11:15 -0600

    Some other styling things

--- src/components/ClearContainer.jsx
+++ src/components/ClearContainer.jsx
@@ -7,12 +7,13 @@ const ClearContainer = ({ children }) => {
                 alignItems: 'center',
                 flexDirection: 'column',
                 backgroundColor: 'rgba(31, 31, 31, 0.5)',
-                borderRadius: '2%',
+                borderRadius: '10px',
                 width: '90vw',
                 boxShadow: '0px 0px 15px green',
                 color: '#7ef97e',
                 margin: '10px auto',
-                alignSelf: 'center'
+                alignSelf: 'center',
+                paddingBottom: '10px'
             }}>
 
                 {children}
