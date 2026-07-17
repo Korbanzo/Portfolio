90e1001678680e302c447126eb434189f45a93ba
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-07-17 15:08:59 -0600

    Tool bar fixing

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -3,7 +3,7 @@ const ToolBar = () => {
 
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', backgroundColor: 'rgb(0, 0, 0, 0.5)', height: '30px', width: '100vw'}}>
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', backgroundColor: 'rgb(0, 0, 0, 0.5)', height: '30px'}}>
 
             <button className="tool_bar_btn" onClick={() => {window.location.href = "#project_browser"}}>Projects</button>
             <button className="tool_bar_btn" onClick={() => {window.location.href = "/contact/contact.html"}}>That one</button>
